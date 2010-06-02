ff78a2011a7d4971ebc0b2d890364cdebd605fc0
Georgi D. Sotirov <gdsotirov@dir.bg>
2010-06-02 10:33:09 +0000

    Initial version

---
+++ geo.hpp
@@ -0,0 +1,146 @@
+/**
+ * @file The modules defines a simple class hierarchy of 2D and 3D shapes.
+ * It's created as an example for Stefan Kanev in a comment on his
+ * publication Structures vs Objects [http://skanev.com/2010/05/30/structs-vs-objects/].
+ * The purpose is to illustrate adding and removing of operations in class
+ * hierarchies used in Object Oriented Programming.
+ */
+
+/* Copyright (C) 2010 Georgi D. Sotirov <gdsotirov@dir.bg>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *
+ * $Id: geo.hpp,v 1.1 2010/06/02 10:33:09 gsotirov Exp $
+ */
+
+#include <cmath>
+
+namespace Geo {
+
+/* Two dimensional space point */
+class Point2D {
+protected:
+  double x;
+  double y;
+
+public:
+  Point2D(double px, double py) : x(px), y(py) {}
+};
+
+/* Three dimensional space point */
+class Point3D: public Point2D {
+protected:
+  double z;
+
+public:
+  Point3D(double px, double py, double pz) : Point2D(px, py), z(pz) {}
+
+  double getX() { return Point2D::x; }
+  double getY() { return Point2D::y; }
+  double getZ() { return z; }
+};
+
+/* General shape */
+class Shape {
+public:
+  virtual double area(void) = 0;
+  virtual double perimeter(void) = 0;
+};
+
+/* Two dimensional shape */
+class Shape2D: public Shape {
+protected:
+  Point2D ref_point;
+
+public:
+  Shape2D(Point2D p) : ref_point(p) {}
+  Shape2D(double px, double py) : ref_point(px, py) {}
+};
+
+/* Three dimensional shpe */
+class Shape3D: public Shape {
+protected:
+  Point3D ref_point;
+
+public:
+  Shape3D(Point3D p) : ref_point(p) {}
+
+  /* remove - not always useful for 3D */
+  double perimeter(void) { return 0; };
+  /* new operation for 3D */
+  virtual double volume(void) = 0;
+};
+
+/* A circle */
+class Circle: public Shape2D {
+private:
+  double radius;
+
+public:
+  Circle(Point2D p, double r) : Shape2D(p), radius(r) {}
+  Circle(double px, double py, double r) : Shape2D(px, py), radius(r) {}
+
+  double getRadius(void) { return radius; }
+
+  double area(void) { return M_PI * radius * radius; }
+  double perimeter(void) { return 2 * M_PI * radius; }
+};
+
+/* A square */
+class Square: public Shape2D {
+private:
+  double side;
+
+public:
+  Square(Point2D p, double s) : Shape2D(p), side(s) {}
+  Square(double x, double y, double s) : Shape2D(x, y), side(s) {}
+
+  double getSide(void) { return side; }
+
+  double area(void) { return side * side; }
+  double perimeter(void) { return side * 4; }
+};
+
+/* A sphere */
+class Sphere: public Shape3D {
+private:
+  Circle cr;
+public:
+  Sphere(Point3D cntr, double r) : Shape3D(cntr), cr(cntr.getX(), cntr.getY(), r) {}
+
+  double getRadius(void) { return cr.getRadius(); }
+
+  double area(void) { return 4 * M_PI * cr.getRadius() * cr.getRadius(); }
+  double perimeter(void) { return cr.perimeter(); }
+  double volume(void) { return 4.0/3.0 * M_PI * cr.getRadius() * cr.getRadius() * cr.getRadius(); }
+};
+
+/* A cube */
+class Cube: public Shape3D {
+private:
+  Square sq;
+public:
+  Cube(Point3D p, double s) : Shape3D(p), sq(p.getX(), p.getY(), s) {}
+
+  double getEdge(void) { return sq.getSide(); }
+
+  double area(void) { return sq.area() * 6; }
+  /* Perimeter by sides or by sides and shape diagonals? */
+  double volume(void) { return sq.getSide() * sq.getSide() * sq.getSide(); }
+};
+
+}
+
---
+++ geoex.cpp
@@ -0,0 +1,64 @@
+/**
+ * @file Test module for the class hierarchy from Geo namespace
+ */
+
+/* Copyright (C) 2010 Georgi D. Sotirov <gdsotirov@dir.bg>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *
+ * $Id: geoex.cpp,v 1.1 2010/06/02 10:33:09 gsotirov Exp $
+ */
+
+#include <iostream>
+
+#include "geo.hpp"
+
+using std::cout;
+using std::endl;
+
+/* Test program */
+int main(int argc, char * argv[]) {
+  Geo::Point2D p2d0(0, 0);
+  Geo::Point3D p3d0(0, 0, 0);
+  Geo::Circle * pCircle = new Geo::Circle(p2d0, 3.5);
+  Geo::Square * pSquare = new Geo::Square(p2d0, 3);
+  Geo::Sphere * pSphere = new Geo::Sphere(p3d0, 3.5);
+  Geo::Cube   * pCube   = new Geo::Cube(p3d0, 3);
+
+  cout << "A circle with radius " << pCircle->getRadius() << endl;
+  cout << " Circle's area is " << pCircle->area() << endl;
+  cout << " Circle's circumference is " << pCircle->perimeter() << endl;
+
+  cout << "A square with side " << pSquare->getSide() << endl;
+  cout << " Square's area is " << pSquare->area() << endl;
+  cout << " Square's perimeter is " << pSquare->perimeter() << endl;
+
+  cout << "A sphere with radius " << pSphere->getRadius() << endl;
+  cout << " Sphere's surface area is " << pSphere->area() << endl;
+  cout << " Sphere's circumference is " << pSphere->perimeter() << endl;
+  cout << " Sphere's volume is " << pSphere->volume() << endl;
+
+  cout << "A cube with edge " << pCube->getEdge() << endl;
+  cout << " Cube's surface area is " << pCube->area() << endl;
+  cout << " Cube's perimeter is " << pCube->perimeter() << endl;
+  cout << " Cube's volume is " << pCube->volume() << endl;
+
+  delete pCube;
+  delete pSphere;
+  delete pSquare;
+  delete pCircle;
+}
+
